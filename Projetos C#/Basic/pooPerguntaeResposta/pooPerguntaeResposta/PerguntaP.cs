6f191dca0879f4373d607873f0bc647dca4e413c
Leonardo Henrique <111592118+leohsantana20@users.noreply.github.com>
2022-09-16 10:09:18 -0300

    Inclusão programa Pergunta e Resposta

---
+++ Projetos C#/Basic/pooPerguntaeResposta/pooPerguntaeResposta/PerguntaP.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooPerguntaeResposta
+{
+    public class PerguntaP:Pergunta
+    {
+        public PerguntaP():base()
+        {
+            this.Texto = "";
+            this.Resposta = "";
+            this.Dica = "";
+        }
+
+        public PerguntaP(String texto, String resposta, String dica):base(texto, resposta)
+        {
+            this.Dica = dica;
+        }
+
+        public String Dica { get; set; }
+    }
+}
---
+++ Projetos C#/Basic/pooPerguntaeResposta/pooPerguntaeResposta/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooPerguntaeResposta
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            PerguntaP p = new PerguntaP();
+            p.Texto = "Qual o seu nome:";
+            p.Resposta = "Meu nome é Leonardo.";
+            p.Dica = "Começa com L.";
+
+            Console.WriteLine("Advinhe a pergunta");
+            Console.WriteLine("Pergunta: " + p.Texto);
+            Console.WriteLine("Dica: " + p.Dica);
+            Console.Write("Resposta: ");
+            String resposta = Console.ReadLine();
+
+            if (p.Resposta.ToUpper() == resposta.ToUpper())
+            {
+                Console.WriteLine("Parabéns vocÊ acertou!");
+            }
+            else
+            {
+                Console.WriteLine("Você errou");
+                Console.WriteLine("A resposta certa é: " + p.Resposta);
+            }
+            Console.ReadKey();
+        }
+    }
+}
