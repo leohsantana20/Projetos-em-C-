2006b59688dd7bf8589455b16f53dda0993d0b55
Leonardo Henrique <111592118+leohsantana20@users.noreply.github.com>
2022-09-16 10:05:13 -0300

    Inclusão projeto para calcular área do retángulo

---
+++ Projetos C#/Basic/CalculoArea/CalculoArea/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculoArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+            Console.WriteLine("Calcula a area de um retangulo");
+
+            Console.WriteLine("Informe a Base: ");
+            retangulo.Base = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Informe a Altura: ");
+            retangulo.Altura = Convert.ToDouble(Console.ReadLine());
+            retangulo.ExibirDados();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Projetos C#/Basic/CalculoArea/CalculoArea/Retangulo.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculoArea
+{
+    public class Retangulo
+    {
+
+        public Retangulo()
+        {
+            this.Altura = 0;
+            this.Base = 0;
+        }
+    
+        // Base Retangulo
+
+        private Double baseret;
+
+        public Double Base
+        {
+            get { return baseret; }
+            set { 
+                if (value >=0)
+                    baseret = value; 
+                else baseret = 0;
+                }
+        }
+
+        // Altura Retangulo
+
+        private Double alturaret;
+
+        public Double Altura
+        {
+            get { return alturaret; }
+            set {
+                if (value >= 0)
+                    alturaret = value;
+                else alturaret = 0;
+                }
+        }
+
+        public Double Area
+        {
+            get { return alturaret * baseret; }
+        }
+
+
+        public void ExibirDados()
+        {
+            Console.WriteLine("Base: "+this.Base);
+            Console.WriteLine("Altura: " + this.Altura);
+            Console.WriteLine("Area: " + this.Area);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
